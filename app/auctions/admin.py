c3934add7400a0b8052d0a0e0b76766c9950651f
Marijn Doeve <marijn@doeve.me>
2021-05-31 10:44:29 +0200

    UserAdmin

--- app/auctions/admin.py
+++ app/auctions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Listing, Category, Bid
+from django.contrib.auth.admin import UserAdmin
+from .models import Listing, Category, Bid, User
+
 
 admin.site.register(Listing)
 admin.site.register(Category)
 admin.site.register(Bid)
+admin.site.register(User, UserAdmin)
